dce864303e65480c75a9cfbd124c21b5d8b855cd
Thomas Croft <thomas.croft@proton.me>
2024-03-06 15:50:32 -0700

    Added: treefmt-nix

--- flake.nix
+++ flake.nix
@@ -1,21 +1,25 @@
 {
   inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixpkgs-unstable";
+
     flake-parts.url = "github:hercules-ci/flake-parts";
     treefmt-nix.url = "github:numtide/treefmt-nix";
   };
-  outputs = inputs@{ flake-parts, ...}:
-    flake-parts.lib.mkFlake { inherit inputs; }{
+  outputs = inputs @ {flake-parts, ...}:
+    flake-parts.lib.mkFlake {inherit inputs;} {
       imports = [
         inputs.treefmt-nix.flakeModule
-      ];      
+      ];
       flake = {
-        
       };
       systems = [
         "x86_64-linux"
       ];
-      perSystem = { config, ...}:{
-        
+      perSystem = {config, ...}: {
+        treefmt = {
+          projectRootFile = "flake.nix";
+          programs.alejandra.enable = true;
+        };
       };
     };
 }
