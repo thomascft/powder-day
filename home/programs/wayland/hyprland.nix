14d755b7a8a73c92f676fc5e3cc8f64c00136621
Thomas Croft <thomas.croft@proton.me>
2024-03-13 08:56:44 -0600

    Added: brightness controls

--- home/programs/wayland/hyprland.nix
+++ home/programs/wayland/hyprland.nix
@@ -97,6 +97,8 @@
         ", XF86AudioPrev, exec, ${pkgs.playerctl}/bin/playerctl previous"
         ", XF86AudioPlay, exec, ${pkgs.playerctl}/bin/playerctl play-pause"
         ", XF86AudioNext, exec, ${pkgs.playerctl}/bin/playerctl next"
+        ", XF86MonBrightnessUp, exec, ${pkgs.brightnessctl}/bin/brightnessctl s +5%"
+        ", XF86MonBrightnessDown, exec, ${pkgs.brightnessctl}/bin/brightnessctl s 5%-"
 
         "$mod, f, fullscreen"
 
