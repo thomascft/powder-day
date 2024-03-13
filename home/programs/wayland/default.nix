5abda6e2469bf854565b1de84d7f7bea3938b497
Thomas Croft <thomas.croft@proton.me>
2024-03-13 08:57:15 -0600

    Added: gtk prefer-dark

--- home/programs/wayland/default.nix
+++ home/programs/wayland/default.nix
@@ -1,8 +1,21 @@
-{...}: {
+{pkgs, ...}: {
   imports = [
     ./hyprland.nix
     ../anyrun
     ../wezterm
     ../../services/wayland
   ];
+
+  gtk = {
+    enable = true;
+    theme = {
+      package = pkgs.adw-gtk3;
+      name = "adw-gtk3-dark";
+    };
+  };
+  dconf.settings = {
+    "org/gnome/desktop/interface" = {
+      color-scheme = "prefer-dark";
+    };
+  };
 }
