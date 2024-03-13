b495ace8c69e09104250da3809c6d887fe0082e8
Thomas Croft <thomas.croft@proton.me>
2024-03-13 08:43:26 -0600

    Fixed: helix config cause I'm dumb

--- home/programs/helix.nix
+++ home/programs/helix.nix
@@ -3,16 +3,16 @@
     enable = true;
     defaultEditor = true;
     settings = {
-      theme = "catppuccin_override";
-    };
-    themes.catppuccin_override = {
       editor = {
         cursorline = true;
         indent-guides = {
           render = true;
         };
       };
-      "inherits" = "catppuccin_mocha";
+       theme = "catppuccin_override";
+    };
+    themes.catppuccin_override = {
+     "inherits" = "catppuccin_mocha";
       "ui.background" = "none";
     };
 
