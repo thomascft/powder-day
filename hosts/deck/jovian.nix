db4093abd61861329b0c1c0c0245f3c8217f523b
Thomas Croft <thomas.croft@proton.me>
2024-03-17 20:59:18 -0600

    Fixed: jovian dekstopsession

--- hosts/deck/jovian.nix
+++ hosts/deck/jovian.nix
@@ -10,7 +10,7 @@
   jovian.steam = {
     enable = true;
     user = "thomas";
-    desktopSession = "Hyprland";
+    desktopSession = "hyprland";
     autoStart = true;
   };
   jovian.devices.steamdeck.enable = true;
