12a1d9f1ffa7f95f68469ab7ec8b1f86d8c41327
Thomas Croft <thomas.croft@proton.me>
2024-03-17 20:58:19 -0600

    Fixed: audio

--- system/services/sound.nix
+++ system/services/sound.nix
@@ -5,16 +5,17 @@
     pulse.enable = true;
   };
 
-  environment.systemPackages = with pkgs; [
-    alsa-tools
-  ];
-  
   systemd.services.audio-fix = {
     description = "Updates HDA verbs to fix speakers";
+    path = with pkgs; [
+      alsa-tools
+    ];
     after = [ "getty.target" ];
-    script = builtins.fetchurl {
-      url = "https://github.com/joshuagrisham/galaxy-book2-pro-linux/raw/main/sound/necessary-verbs.sh";
-      sha256 = "sha256:030xadxg35hacbw085hbrb86b0m8zr7cl1i1qm6vwxrczqwxqnax";
-    };
+    script = builtins.readFile ( 
+      builtins.fetchurl {
+        url = "https://github.com/joshuagrisham/galaxy-book2-pro-linux/raw/main/sound/necessary-verbs.sh";
+        sha256 = "sha256:030xadxg35hacbw085hbrb86b0m8zr7cl1i1qm6vwxrczqwxqnax";
+      }
+    );
   };
 }
