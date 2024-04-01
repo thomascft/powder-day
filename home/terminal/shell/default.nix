b87405154b3c6159b390ccf25b3a6e9cb5108864
Thomas Croft <thomas.croft@proton.me>
2024-04-01 12:47:46 -0600

    Changed: prompt symbols

--- home/terminal/shell/default.nix
+++ home/terminal/shell/default.nix
@@ -16,12 +16,16 @@
     enable = true;
     settings = {
       format = ''
-        [](fg:green)[](bold fg:black bg:green)[](fg:green)
+        $directory $character
       '';
       character = {
-        success_symbol = "";
+        success_symbol = "[](bold green)";
+        error_symbol = "[](bold red)";
       };
-    };
+      directory = {
+        format = "[](fg:green)[$path](bold fg:black bg:green)[](fg:green)";
+      };
+    };  
   };
 
   programs.carapace = {
