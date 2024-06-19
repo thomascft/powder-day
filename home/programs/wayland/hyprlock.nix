36f8ee08928e3c37e6d33000516621fa1693d0f2
Thomas Croft <thomas.croft@proton.me>
2024-06-19 10:02:35 -0600

    Updated: hyprlock config

--- home/programs/wayland/hyprlock.nix
+++ home/programs/wayland/hyprlock.nix
@@ -3,28 +3,26 @@
   config,
   ...
 }: {
-  imports = [
-    inputs.hyprlock.homeManagerModules.default
-  ];
+  imports = [];
 
   programs.hyprlock = {
     enable = true;
-    labels = [];
-    input-fields = [
-      {
-        fade_on_empty = false;
-        outer_color = "rgb(205, 214, 244)";
-        font_color = "rgb(205, 214, 244)";
-        inner_color = "rgb(30, 30, 46)";
-      }
-    ];
-    backgrounds = [
-      {
-        path = "${config.theme.wallpaper}";
-
-        blur_passes = 3;
-        blur_size = 12;
-      }
-    ];
+    settings = {
+      input-field = [
+        {
+          fade_on_empty = false;
+          outer_color = "rgb(205, 214, 244)";
+          font_color = "rgb(205, 214, 244)";
+          inner_color = "rgb(30, 30, 46)";
+        }
+      ];
+      background = [
+        {
+          path = "${config.theme.wallpaper}";
+          blur_passes = 3;
+          blur_size = 12;
+        }
+      ];
+    };
   };
 }
