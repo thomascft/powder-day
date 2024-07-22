1470ff63a6d126702a0384176745714ffa3d4b20
Thomas Croft <thomas.croft@proton.me>
2024-07-22 12:45:46 -0600

    Changed: hyprlock config

--- home/hyprland/hyprlock.nix
+++ home/hyprland/hyprlock.nix
@@ -10,17 +10,31 @@
     settings = {
       input-field = [
         {
+          size = "200, 50";
+          position = "0, -200";
+          outline_thickness = 0;
           fade_on_empty = false;
-          outer_color = "rgb(205, 214, 244)";
+          placeholder_text = "<span font_desc='Maple Mono NF Italic'>Input Password...</span>";
           font_color = "rgb(205, 214, 244)";
           inner_color = "rgb(30, 30, 46)";
+        }
+      ];
+      label = [
+        {
+          text = "$TIME";
+          position = "0, 0";
+          halign = "center";
+          valign = "center";
+          font_size = 80;
+          font_family = "Maple Mono NF Italic";
+          color = "rgb(205, 214, 244)";
         }
       ];
       background = [
         {
           path = "${config.theme.wallpaper}";
           blur_passes = 3;
-          blur_size = 12;
+          blur_size = 4;
         }
       ];
     };
