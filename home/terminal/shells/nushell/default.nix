49bf46a70e0aeaf1bfc09558acb84a5f73d6f555
Thomas Croft <thomas.croft@proton.me>
2024-07-24 14:18:13 -0600

    Added: nushell var workaround

--- home/terminal/shells/nushell/default.nix
+++ home/terminal/shells/nushell/default.nix
@@ -1,6 +1,7 @@
-{...}: {
+{config, ...}: {
   programs.nushell = {
     enable = true;
     configFile.source = ./config.nu;
+        environmentVariables = builtins.mapAttrs (name: value: "\"${builtins.toString value}\"") config.home.sessionVariables;
   };
 }
