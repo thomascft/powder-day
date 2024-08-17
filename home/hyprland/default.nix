499734d495dc69bd018bf8e88cd55a9a488dc245
Thomas Croft <thomas.croft@proton.me>
2024-08-16 18:37:38 -0600

    Fixed: hyprland workspaces function

--- home/hyprland/default.nix
+++ home/hyprland/default.nix
@@ -129,18 +129,18 @@
         "$mod, j, movefocus, d"
         "$mod, k, movefocus, u"
         "$mod, l, movefocus, r"
-     ] + (
+     ] ++ (
       builtins.concatLists (builtins.genList (
         x: let
           ws = let
             c = (x + 1) / 10;
           in
-            builtins.toString (x +1 - (c * 10));
+            builtins.toString (x + 1 - (c * 10));
         in [
           "$mod, ${ws}, workspace, ${toString (x + 1)}"
           "$mod SHIFT, ${ws}, movetoworkspace, ${toString (x + 1)}"
         ]
-      ))
+      ) 10 )
     );
     };
   };
