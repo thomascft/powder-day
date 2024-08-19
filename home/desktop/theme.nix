448422f55aa5e1d6ad2d7ed737d6d27253c23794
Thomas Croft <thomas.croft@proton.me>
2024-08-19 09:34:52 -0600

    Theme: set cursor theme

--- home/desktop/theme.nix
+++ home/desktop/theme.nix
@@ -4,8 +4,19 @@
       color-scheme = "prefer-dark";
     };
   };
+  home.sessionVariables = {
+    "HYPRCURSOR_THEME" = "catppuccin-mocha-dark-cursors";
+    "HYPRCURSOR_SIZE" = 24;
+  };
 
+
+  home.pointerCursor = {
+    package = pkgs.catppuccin-cursors.mochaDark;
+    name = "catppuccin-mocha-dark-cursors";
+    size = 24;
+  };
   fonts.fontconfig.enable = true;
+
   home.packages = with pkgs; [
     iosevka
   ];
