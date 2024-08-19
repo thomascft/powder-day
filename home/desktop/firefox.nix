129a1634bb54af7e44e76c4425f093c51ed30e68
Thomas Croft <thomas.croft@proton.me>
2024-08-19 07:48:39 -0600

    FireFox: default profile + search engines

--- home/desktop/firefox.nix
+++ home/desktop/firefox.nix
@@ -1,3 +1,53 @@
-{pkgs, ...}:{
-  programs.firefox.enable = true;
+{...}:{
+  programs.firefox = {
+    enable = true;
+    profiles = {
+      default = {
+        id = 0;
+        name = "default";
+        isDefault = true;
+        search = {
+          force = true;
+          default = "Google";
+          order = [ "Google" ];
+          engines = {
+            "Nix Packages" = {
+              urls = [
+                {
+                  template = "https://search.nixos.org/packages";
+                  params = [
+                    { name = "type"; value = "packages"; }
+                    { name = "query"; value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              definedAliases = [ "@np" ];
+            };
+            "GitHub" = {
+              urls = [
+                {
+                  template = "https://github.com/search";
+                  params = [
+                    { name = "q"; value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              definedAliases = [ "@gh" ];
+            };
+            "SourceGraph" = {
+              urls = [
+                {
+                  template = "https://sourcegraph.com/search";
+                  params = [
+                    { name = "q"; value = "{searchTerms}"; }
+                  ];
+                }
+              ];
+              definedAliases = [ "@sg" ];
+            };
+          };
+        };
+      };
+    };
+  };
 }
