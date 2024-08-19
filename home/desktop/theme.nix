1b991daf6927dc791c694fe6e6979f8139d6ac91
Thomas Croft <thomas.croft@proton.me>
2024-08-19 09:47:40 -0600

    theme: gtk adw dark

--- home/desktop/theme.nix
+++ home/desktop/theme.nix
@@ -9,12 +9,20 @@
     "HYPRCURSOR_SIZE" = 24;
   };
 
-
   home.pointerCursor = {
     package = pkgs.catppuccin-cursors.mochaDark;
     name = "catppuccin-mocha-dark-cursors";
     size = 24;
   };
+
+  gtk = {
+    enable = true;
+    theme = {
+      name = "adw-gtk3-dark";
+      package = pkgs.adw-gtk3;
+    };
+  };
+
   fonts.fontconfig.enable = true;
 
   home.packages = with pkgs; [
