32d369852564914d07de6d4b0d94d2d96131c2be
Thomas Croft <thomas.croft@proton.me>
2024-09-02 22:24:36 -0600

    home/desktop/hyprland: added xdg portal config

--- home/desktop/hyprland.nix
+++ home/desktop/hyprland.nix
@@ -100,4 +100,17 @@
     );
     };
   };
+
+  xdg.portal = {
+    enable = true;
+    xdgOpenUsePortal = true;
+    config = {
+      common.default = ["gtk"];
+      hyprland.default = ["gtk" "hyprland"];
+    };
+
+    extraPortals = [
+      pkgs.xdg-desktop-portal-gtk
+    ];
+  };
 }
