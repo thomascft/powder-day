74577d0744b61d80f88a79965f1b43307d568bd3
Thomas Croft <thomas.croft@proton.me>
2024-09-03 13:16:38 -0600

    system/desktop: move hyprland

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -1,22 +1,5 @@
-{inputs, config, pkgs, ...}:{
+{...}:{
   imports = [
-    inputs.hyprland.nixosModules.default
-    ./sound.nix
+    ./hyprland
   ];
-
-  environment.sessionVariables.NIXOS_OZONE_WL = "1";
-  programs.hyprland.enable = true;
-  programs.hyprland.package = inputs.hyprland.packages.${pkgs.system}.hyprland;
-  
-  # Auto-login
-
-  services.greetd = {
-    enable = true;
-    settings = {
-      default_session = {
-        command = "${pkgs.greetd.greetd}/bin/agreety --cmd ${config.programs.hyprland.package}/bin/Hyprland";
-        user = "thomas";
-      };
-    };
-  };
 }
