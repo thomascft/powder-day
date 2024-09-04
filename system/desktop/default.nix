6ba3e16a4cc551043981e202ebf58f207e5d19e5
Thomas Croft <thomas.croft@proton.me>
2024-09-03 20:31:32 -0600

    Revert "system/desktop: move hyprland"
    This reverts commit 74577d0744b61d80f88a79965f1b43307d568bd3.

--- system/desktop/default.nix
+++ system/desktop/default.nix
@@ -1,5 +1,22 @@
-{...}:{
+{inputs, config, pkgs, ...}:{
   imports = [
-    ./hyprland
+    inputs.hyprland.nixosModules.default
+    ./sound.nix
   ];
+
+  environment.sessionVariables.NIXOS_OZONE_WL = "1";
+  programs.hyprland.enable = true;
+  programs.hyprland.package = inputs.hyprland.packages.${pkgs.system}.hyprland;
+  
+  # Auto-login
+
+  services.greetd = {
+    enable = true;
+    settings = {
+      default_session = {
+        command = "${pkgs.greetd.greetd}/bin/agreety --cmd ${config.programs.hyprland.package}/bin/Hyprland";
+        user = "thomas";
+      };
+    };
+  };
 }
